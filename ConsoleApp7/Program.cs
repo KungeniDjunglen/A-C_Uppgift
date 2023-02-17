70a076ad4a2bad8a5b82fdae8f656e455e34e68c
Admin <Admin@TEB127010>
2023-02-17 10:23:01 +0100

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class Program
+{
+    static double FtC(int Grader)
+    {
+        return (Grader - 32) / 1.8;
+        
+    }
+    static double CtF(int grader)
+    {
+        return (32 + grader) * 1.8;
+    }
+    static void Main()
+    {
+        Console.WriteLine("Välj åt vilket håll du vill omvandla:");
+        Console.WriteLine("1. Farenheit till celsius.");
+        Console.WriteLine("2. Celsius till Farenheit.");
+        int val = int.Parse(Console.ReadLine());
+
+
+        switch (val)
+        {
+            case 1:
+                Console.Write("Hur många grader Farenheit ? ");
+                int graderF = int.Parse(Console.ReadLine());
+                double Celsius = FtC(graderF);
+
+                Console.WriteLine("Det är " + Celsius + " grader celsius");
+                break;
+            case 2:
+                Console.Write("Hur många grader Celsius ? ");
+                int graderC = int.Parse(Console.ReadLine());
+                double Farenheit = CtF(graderC);
+
+                Console.WriteLine("Det är " + Farenheit + " grader farenheit");
+                break;
+        }
+    }
+}
